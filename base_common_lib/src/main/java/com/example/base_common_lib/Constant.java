f32d0fbcf415df3f66f5a1ba865828c0d08a5ba0
gjszs <690617105@qq.com>
2020-06-20 01:11:47 +0800

    测试提交

--- base_common_lib/src/main/java/com/example/base_common_lib/Constant.java
+++ base_common_lib/src/main/java/com/example/base_common_lib/Constant.java
@@ -1,5 +1,5 @@
 package com.example.base_common_lib;
 
 public class Constant {
-    public static final String kobe_image_url = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=245785532,241083071&fm=26&gp=0.jpg";
+     public static final String kobe_image_url = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=245785532,241083071&fm=26&gp=0.jpg";
 }
