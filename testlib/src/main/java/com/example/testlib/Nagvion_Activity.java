fdcd121840562c0dc67375d5050514f60ccfa561
gjs <690617105@qq.com>
2020-06-22 17:55:52 +0800

    remove_luka_jpg

--- testlib/src/main/java/com/example/testlib/Nagvion_Activity.java
+++ testlib/src/main/java/com/example/testlib/Nagvion_Activity.java
@@ -57,6 +57,7 @@ public class Nagvion_Activity extends BaseTitleActivity {
         return R.layout.activity_nagvion;
     }
 
+    @SuppressLint("CheckResult")
     @Override
     protected void init(Bundle savedInstanceState) {
 
